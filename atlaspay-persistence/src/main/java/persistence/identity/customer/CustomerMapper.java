64f57d294884c7f35a787782234e2b78f6059b22
OlamidotunIY <olamidotun225@gmail.com>
2026-07-22 23:05:34 +0100

    add: include toJpaEntity method and toDomain method

--- atlaspay-persistence/src/main/java/persistence/identity/customer/CustomerMapper.java
+++ atlaspay-persistence/src/main/java/persistence/identity/customer/CustomerMapper.java
@@ -1,4 +1,42 @@
 package persistence.identity.customer;
 
-public class CustomerMapper {
+import core.identity.Customer;
+import core.identity.valueobject.*;
+
+public final class CustomerMapper {
+    public CustomerJpaEntity toJpaEntity(Customer domain) {
+        return new CustomerJpaEntity(
+                domain.id().value(),
+                domain.getCompanyId().value(),
+                domain.getPersonalDetails().fullName(),
+                domain.getPersonalDetails().dateOfBirth(),
+                domain.getPersonalDetails().address().line1(),
+                domain.getPersonalDetails().address().line2(),
+                domain.getPersonalDetails().address().city(),
+                domain.getPersonalDetails().address().postalCode(),
+                domain.getPersonalDetails().address().countryCode(),
+                domain.getKycTier(),
+                domain.getKycStatus()
+        );
+    }
+
+    public Customer toDomain(CustomerJpaEntity entity) {
+        return Customer.rehydrate(
+                new CustomerId(entity.getId()),
+                new CompanyId(entity.getCompanyId()),
+                new PersonalDetails(
+                        entity.getFullName(),
+                        entity.getDateOfBirth(),
+                        new Address(
+                                entity.getAddressLine1(),
+                                entity.getAddressLine2(),
+                                entity.getCity(),
+                                entity.getPostalCode(),
+                                entity.getCountryCode()
+                        )
+                ),
+                entity.getKycTier(),
+                entity.getKycStatus()
+        );
+    }
 }
