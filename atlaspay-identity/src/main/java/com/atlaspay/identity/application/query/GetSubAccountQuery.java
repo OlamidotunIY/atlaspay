0ffe235dbfd0da9ea69d49dc040b96369a0782c2
OlamidotunIY <olamidotun225@gmail.com>
2026-08-13 15:27:21 +0100

    refactor(identity): update queries with Long IDs and integration terminology

--- atlaspay-identity/src/main/java/com/atlaspay/identity/application/query/GetSubAccountQuery.java
+++ atlaspay-identity/src/main/java/com/atlaspay/identity/application/query/GetSubAccountQuery.java
@@ -1,9 +1,7 @@
 package com.atlaspay.identity.application.query;
 
-import com.atlaspay.shared.domain.id.MerchantId;
-import com.atlaspay.shared.domain.id.SubAccountId;
 
 public record GetSubAccountQuery(
-    MerchantId merchantId,
-    SubAccountId subAccountId
+    Long merchantId,
+    Long subAccountId
 ) {}
--- atlaspay-identity/src/main/java/com/atlaspay/identity/application/query/ListApiKeysQuery.java
+++ atlaspay-identity/src/main/java/com/atlaspay/identity/application/query/ListApiKeysQuery.java
@@ -1,7 +1,6 @@
 package com.atlaspay.identity.application.query;
 
-import com.atlaspay.shared.domain.id.MerchantId;
 
 public record ListApiKeysQuery(
-    MerchantId merchantId
+    Long merchantId
 ) {}
