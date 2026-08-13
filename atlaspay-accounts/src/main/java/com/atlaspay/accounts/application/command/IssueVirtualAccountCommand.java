a438935c944cb44b2b8638e24cca3e949bf7c76e
OlamidotunIY <olamidotun225@gmail.com>
2026-08-13 15:27:24 +0100

    refactor(accounts): update commands to use customer code and integration terminology

--- atlaspay-accounts/src/main/java/com/atlaspay/accounts/application/command/IssueVirtualAccountCommand.java
+++ atlaspay-accounts/src/main/java/com/atlaspay/accounts/application/command/IssueVirtualAccountCommand.java
@@ -1,13 +1,13 @@
 package com.atlaspay.accounts.application.command;
 
-import com.atlaspay.accounts.domain.model.OwnerType;
 import com.atlaspay.shared.annotation.IdempotencyKey;
 import com.atlaspay.shared.usecase.Command;
 
 public record IssueVirtualAccountCommand(
-        @IdempotencyKey String idempotencyKey, 
-        String ownerId, 
-        OwnerType ownerType, 
-        String bankName
+        Long integration,
+        String customerCode,
+        String accountName,
+        String bankName,
+        @IdempotencyKey String idempotencyKey
 ) implements Command {
 }
